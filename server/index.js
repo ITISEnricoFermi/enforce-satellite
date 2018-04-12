c2cc338c4437ba6e151ae66b584967a7995b2869
n4y0n <alfredomontada0@gmail.com>
2018-04-12 14:28:56 +0200

    Update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const readline = require('readline');
 const { XBee } = require('./deps/XBee');
-const xbee = new XBee(process.env.SATSERIAL || 'COM4', 115200);
+const xbee = new XBee(process.env.SATSERIAL || 'COM5', 115200);
 /*{ sendData: function(data) { console.log(data) }, on: function(e, cb) { cb(e) } };*/
 
 const l = readline.createInterface({
@@ -14,5 +14,4 @@ l.on('line', d => {
 
 xbee.on('data', d => {
   console.log(d);
-  xbee.sendData(d);
 })
