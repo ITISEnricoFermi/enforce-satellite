b4611aa3e9466f79e4ad61109285ab0600d6b335
Revanee <hkamenov123@gmail.com>
2018-04-17 14:02:27 +0200

    changed

--- server/test.js
+++ server/test.js
@@ -1,9 +1,5 @@
-var BNO055 = require('bno055');
- 
-var imu = new BNO055({device: '/dev/i2c-0'});
-console.log(imu.options)
- 
-imu.beginNDOF(function() {
-    console.info('imu running');
-    console.log(imu.getCalibrationStatus)
-});
+imu = require('./deps/imu')()
+
+imu.startReading()
+
+imu.on('quaternion', console.log)
