22be0f475f7fc8ec052847a41a43e2b9d309981a
Revanee <hkamenov123@gmail.com>
2018-04-17 16:12:11 +0200

    Changed

--- server/test.js
+++ server/test.js
@@ -1,6 +1,13 @@
-const imu = require('deps/imu')()
-const sensors = require('lib/sensors')({imu: imu})
+const imu = require('./deps/imu')()
+const sensors = require('./lib/sensors')({imu: imu})
+const motors = require('./deps/motors')()
+const pilot = require('./lib/pilot')(motors)
+const targeter = require('./lib/targeter')()
 
-sensors.on('quaternion', console.log)
+sensors.on('quaternion', (data) => {
+    targeter.setCurrentOrientation(data.z)
+})
+
+pilot.enableAutopilot(targeter)
 
 //setTimeout(() => imu.stopReading(), 5000)
