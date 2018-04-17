5bc15ee589715e7e660aa6ce5fe28ba7b07d9944
Revanee <hkamenov123@gmail.com>
2018-04-17 14:02:15 +0200

    Created IMU class

---
+++ server/deps/imu.js
@@ -0,0 +1,32 @@
+const { EventEmitter } = require('events')
+const BNO055 = require('bno055')
+
+class IMU extends EventEmitter {
+    constructor() {
+        super()
+        this.enabled = false
+        this.imu = new BNO055({device: '/dev/i2c-0'})
+        this.imu.beginNDOF(() => {
+            this.enabled = true
+            console.log('imu enabled')
+        })
+        this.quaternionChecker = true
+    }
+
+    checkQuaternion() {
+        imu.getQuaternion((data) => {
+            this.emit('quaternion', data)
+            if(this.quaternionChecker) this.checkQuaternion()
+        })
+    }
+
+    startReading() {
+        this.checkQuaternion()
+    }
+
+    stopReading() {
+        this.quaternionChecker = false
+    }
+}
+
+module.exports = () => new IMU()
