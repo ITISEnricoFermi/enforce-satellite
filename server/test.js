9dc36745e282fcb82a6cdbead1102f68ec0d1193
Revanee <hkamenov123@gmail.com>
2018-04-17 11:44:30 +0200

    modified

--- server/test.js
+++ server/test.js
@@ -1,22 +1,9 @@
 var BNO055 = require('bno055');
  
-var async = require('async');
- 
 var imu = new BNO055({device: '/dev/i2c-0'});
 console.log(imu.options)
  
 imu.beginNDOF(function() {
     console.info('imu running');
- 
-    setInterval(function() {
-        async.series({
-            calibrationStatus: imu.getCalibrationStatus.bind(imu),
-            quaternion: imu.getQuaternion.bind(imu),
-            euler: imu.getEuler.bind(imu),
-            linearAcceleration: imu.getLinearAcceleration.bind(imu)
-        },
-        function(err, results) {
-            console.info( 'imu: ', JSON.stringify(results) );
-        });
-    }, 1000);
+    console.log(imu.getCalibrationStatus)
 });
