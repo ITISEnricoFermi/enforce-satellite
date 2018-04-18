ebc142e0caeb94f30f39ef83de73e958fa264bf2
n4y0n <alfredomontada0@gmail.com>
2018-04-18 12:16:13 +0200

    Simple test

--- server/test.js
+++ server/test.js
@@ -3,12 +3,14 @@
 //const motors = require('./deps/motors')()
 //const pilot = require('./lib/pilot')(motors)
 //const targeter = require('./lib/targeter')()
-const xbee = require("./mock/XBee")()
+//const xbee = require("./mock/XBee")()
+const xbee = new (require("./deps/XBee").XBee)("/dev/ttyS1", 115200)
 const comms = require("./lib/comms")(xbee)
 
 comms.send("loc", {test: 0342})
 
-comms.on("")
+comms.onE("data", d => console.log(d))
+comms.onE("command", d => console.log(d))
 
 // sensors.on('quaternion', (data) => {
 //     targeter.setCurrentOrientation(data.z)
