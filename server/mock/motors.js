898a959ac730e24e99c2ecc4b271f68f57683266
Revanee <hkamenov123@gmail.com>
2018-04-18 08:18:37 +0200

    Refactor

---
+++ server/mock/motors.js
@@ -0,0 +1,19 @@
+class MotorsController {
+    constructor() {
+        this.debugEnabled = false
+    }
+
+    setDebug(state) {
+        this.debugEnabled = state
+    }
+
+    setRight(state) {
+        if (this.debugEnabled) console.log('Right motor set to: ' + state)
+    }
+
+    setLeft(state) {
+        if (this.debugEnabled) console.log('Left motor set to: ' + state)
+    }
+}
+
+module.exports = new MotorsController()
