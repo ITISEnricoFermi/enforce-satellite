513b1a221938950836e81e7e53a88459dbbcbfc1
n4y0n <alfredomontada0@gmail.com>
2018-04-18 13:30:24 +0200

    node-nan

--- server/test.js
+++ server/test.js
@@ -1,10 +1,11 @@
 const xbee = new (require("module-xbee").XBee)(process.env.XBEEPORT || "/dev/ttyS2", 115200)
-const imu = require('./deps/imu')()
+//const imu = require('./deps/imu')()
+const gps = require("node-nan")
 //const motors = require('./deps/motors')()
 //const pilot = require('./lib/pilot')(motors)
 //const targeter = require('./lib/targeter')()
-
-const sensorIMU = require('./lib/sensors')({imu})
+gps.Start("/dev/ttyS1")
+//const sensorIMU = require('./lib/sensors')({imu})
 const comms = require("./lib/comms")(xbee)
 
 comms.onE("data", d => {
@@ -15,6 +16,11 @@ sensors.on('quaternion', (data) => {
   comms.send("ori", data)
 })
 
+setTimeout(() => {
+  gps.GetData((err, data) => {
+    console.log(data)
+  })
+}, 1000)
 // pilot.enableAutopilot(targeter)
 
 
