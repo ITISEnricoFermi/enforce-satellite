e39bd6a14df74edafd2b2f42377786e77eedeed9
n4y0n <alfredomontada0@gmail.com>
2018-04-18 13:20:34 +0200

    test

--- server/test.js
+++ server/test.js
@@ -1,30 +1,32 @@
-//const imu = require('./deps/imu')()
-//const sensors = require('./lib/sensors')({imu})
+const xbee = new (require("module-xbee").XBee)(process.env.XBEEPORT || "/dev/ttyS2", 115200)
+const imu = require('./deps/imu')()
 //const motors = require('./deps/motors')()
 //const pilot = require('./lib/pilot')(motors)
 //const targeter = require('./lib/targeter')()
-//const xbee = require("./mock/XBee")()
-const xbee = new (require("./deps/XBee").XBee)("/dev/ttyS2", 115200)
+
+const sensorIMU = require('./lib/sensors')({imu})
 const comms = require("./lib/comms")(xbee)
+
+comms.onE("data", d => {
+  console.log(d)
+})
+
+sensors.on('quaternion', (data) => {
+  comms.send("ori", data)
+})
+
+// pilot.enableAutopilot(targeter)
+
+
+//setTimeout(() => imu.stopReading(), 5000)
+
+
 
 const line = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-comms.onE("data", d => {
-  console.log(d)
-})
-
 line.on("line", line => {
   comms.send(null ,line)
 })
-
-// sensors.on('quaternion', (data) => {
-//     targeter.setCurrentOrientation(data.z)
-// })
-
-// pilot.enableAutopilot(targeter)
-
-
-//setTimeout(() => imu.stopReading(), 5000)
