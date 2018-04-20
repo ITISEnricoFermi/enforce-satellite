098e64200d7ce2d4506bb0db6355337ce2823be8
Revanee <hkamenov123@gmail.com>
2018-04-20 10:42:29 +0200

    Distance now calculated properly

--- server/lib/targeter.js
+++ server/lib/targeter.js
@@ -52,16 +52,33 @@ class Targeter extends EventEmitter {
         return delta
     }
 
-    getTargetDistance() {
-        return Math.sqrt(
-            Math.pow(Math.abs(this.target.x - this.currentPosition.x), 2) +
-            Math.pow(Math.abs(this.target.y - this.currentPosition.y), 2))
-    }
+    // getTargetDistance() {
+    //     return Math.sqrt(
+    //         Math.pow(Math.abs(this.target.x - this.currentPosition.x), 2) +
+    //         Math.pow(Math.abs(this.target.y - this.currentPosition.y), 2))
+    // }
 
     getTurnDirection() {
         const delta = this.getTargetDirectionDelta()
         if (Math.abs(delta) < this.deadzone) return 0
         return delta > 0 ? 1 : -1
+    }
+
+    getTargetDistance(){  // generally used geo measurement function
+        const lat1 = this.currentPosition.x
+        const lon1 = this.currentPosition.y
+        const lat2 = this.target.x
+        const lon2 = this.target.y
+
+        var R = 6378.137; // Radius of earth in KM
+        var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+        var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+        Math.sin(dLon/2) * Math.sin(dLon/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c;
+        return d * 1000; // meters
     }
 }
 
