f76eef2fee10479ff72e3a5038e6baf519ed9bd3
Ernesto Montada Romero <e.montada0@gmail.com>
2018-04-22 01:23:00 +0200

    motor status

--- server/deps/motors.js
+++ server/deps/motors.js
@@ -27,11 +27,11 @@ class MotorsController {
     }
 
     setRight(state) {
-        this.right.writeSync(state ? 0 : 1)
+        this.right.writeSync(state ? 1 : 0)
     }
 
     setLeft(state) {
-        this.left.writeSync(state ? 0 : 1)
+        this.left.writeSync(state ? 1 : 0)
     }
 }
 
