a7d1c8f764b861007059f70ed2a90377473afeb8
Ernesto Montada Romero <e.montada0@gmail.com>
2018-04-22 00:49:44 +0200

    cammera.kill

--- server/deps/Camera.js
+++ server/deps/Camera.js
@@ -39,3 +39,16 @@ class Camera {
 }
 
 module.exports = Camera
+
+
+
+/* 
+# [Varables]
+source_stream="http://localhost:8080/?action=stream"
+destination_directory="/home/sd"
+destination_file="cncjs-recording_$(date +'%Y%m%d_%H%M%S').mpeg"
+
+# Recored Stream w/ ffmpeg
+ffmpeg -f mjpeg -re -i "${source_stream}" -q:v 10 "${destination_directory}/${destination_file}"
+
+*/
--- server/main.js
+++ server/main.js
@@ -32,17 +32,6 @@ const sensors = new SENSORS({
     gps
 })
 
-/* 
-# [Varables]
-source_stream="http://localhost:8080/?action=stream"
-destination_directory="/home/sd"
-destination_file="cncjs-recording_$(date +'%Y%m%d_%H%M%S').mpeg"
-
-# Recored Stream w/ ffmpeg
-ffmpeg -f mjpeg -re -i "${source_stream}" -q:v 10 "${destination_directory}/${destination_file}"
-
-*/
-
 archiver.beginMission()
 
 comms.on("command", (commandString) => {
@@ -146,5 +135,5 @@ sensors.on("location", d => {
 pilot.enableAutopilot(target)
 
 process.on('SIGINT', () => {
-    
+    camera.kill()
 })
