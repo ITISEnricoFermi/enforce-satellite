09e256b1808a3b138d3ea3d155001936cae4f140
Ernesto Montada Romero <e.montada0@gmail.com>
2018-05-08 17:05:31 +0200

    Enforce cli testing

--- server/main.js
+++ server/main.js
@@ -13,16 +13,17 @@ const SENSORS = require('./lib/sensors')
 const ARCHIVER = require("./lib/archiver")
 const TARGETER = require("./lib/targeter")
 const PILOT = require("./lib/pilot")
+const ENFORCE_CLI = require("enforce-cli")
 
 const motors = new MOTORS()
-const target = new TARGETER({
-    x: 10.932707,
-    y: 44.649381
+const targeter = new TARGETER({
+	x: 10.932707,
+	y: 44.649381
 })
 
-target.setPosition({
-    x: 10.929782,
-    y: 44.649649
+targeter.setPosition({
+	x: 10.929782,
+	y: 44.649649
 })
 
 const xbee = new XBee(process.env.XBEEPORT || "/dev/ttyS2", 115200)
@@ -36,114 +37,93 @@ const comms = new COMMS(xbee)
 const pilot = new PILOT(motors)
 const archiver = new ARCHIVER(storage)
 const sensors = new SENSORS({
-    thp,
-    imu,
-    gps
+	thp,
+	imu,
+	gps
+})
+const cli = new ENFORCE_CLI(comms, {
+	sensors,
+	motors,
+	camera
 })
 
 archiver.beginMission()
 
 comms.on("command", (commandString) => {
-    debug('Received command: ' + commandString)
-    switch (commandString[0]) {
-        case 'm':
-            let motorState
-
-            if (commandString[2] === '0') motorState = true
-            if (commandString[2] === '1') motorState = false
-
-            if (commandString[1] === 'r') motors.setRight(motorState)
-            if (commandString[1] === 'l') motors.setLeft(motorState)
-
-            break;
-
-        case 'g':
-            if (sensors.SENSORS.gps) {
-                if (commandString[1] === '0') sensors.gpsOff()
-                if (commandString[1] === '1') sensors.gpsOn()
-            }
-            break;
-        case 'x':
-            let locarr = commandString.split(",")
-            target.setTarget({
-                x: parseFloat(locarr[1]),
-                y: parseFloat(locarr[0].slice(1))
-            })
-            break;
-        case 'c':
-            if (commandString[1] === 's') {
-                if (commandString[2] === '0') camera.kill()
-                if (commandString[2] === '1') camera.start()
-            } else {
-                if (commandString[1] === '0') camera.kill()
-                if (commandString[1] === '1') camera.start()
-            }
-            break;
-        case 'i':
-            if (sensors.SENSORS.imu) {
-                if (commandString[1] === '0') sensors.imuOff()
-                if (commandString[1] === '1') sensors.imuOn()
-            }
-            break;
-        case 'b':
-            if (sensors.SENSORS.thp) {
-                if (commandString[1] === '0') sensors.thpOff()
-                if (commandString[1] === '1') sensors.thpOn()
-            }
-            break;
-        case 's':
-            comms.send("status", Object.assign({}, sensors.status(), motors.getStatus(), pilot.status()))
-            break;
-        case 'p':
-            if (commandString[1] === '0') pilot.disableAutopilot()
-            if (commandString[1] === '1') pilot.enableAutopilot()
-            comms.send("status", Object.assign({}, sensors.status(), motors.getStatus(), pilot.status()))
-            break;
-        default:
-            comms.send(`Command ${commandString} not defined.`)
-            break;
-    }
+	debug('Received command: ' + commandString)
+	switch (commandString[0]) {
+		case 'x':
+			let locarr = commandString.split(",")
+			targeter.setTarget({
+				x: parseFloat(locarr[1]),
+				y: parseFloat(locarr[0].slice(1))
+			})
+			break;
+		case 'p':
+			if (commandString[1] === '0') pilot.disableAutopilot()
+			if (commandString[1] === '1') pilot.enableAutopilot()
+			comms.send("status", Object.assign({}, sensors.status(), motors.getStatus(), pilot.status()))
+			break;
+		default:
+			comms.send(`Command ${commandString} not defined.`)
+			break;
+	}
 })
 
 sensors.on("quaternion", d => {
-    comms.send("orientation", d)
-    archiver.saveData({orientation: d})
+	comms.send("orientation", d)
+	archiver.saveData({
+		orientation: d
+	})
 })
 
 sensors.on("euler", d => {
-    target.setOrientation(d.heading)
+	targeter.setOrientation(d.heading)
 })
 
 sensors.on("temperature", d => {
-    comms.send("temperature", d)
-    archiver.saveData({temperature: d})
+	comms.send("temperature", d)
+	archiver.saveData({
+		temperature: d
+	})
 })
 
 sensors.on("humidity", d => {
-    comms.send("humidity", d)
-    archiver.saveData({humidity: d})
+	comms.send("humidity", d)
+	archiver.saveData({
+		humidity: d
+	})
 })
 
 sensors.on("pressure", d => {
-    comms.send("pressure", d)
-    archiver.saveData({pressure: d})
+	comms.send("pressure", d)
+	archiver.saveData({
+		pressure: d
+	})
 })
 
 sensors.on("position", d => {
-    archiver.saveData({position: d})
-//    target.setPosition({
-//        x: d.longitude,
-//        y: d.latitude
-//    })
-    delete d.course
-    comms.send("position", d)
-    comms.send("target", Object.assign({}, {target: target.target}, {directionD: target.getTargetDirectionDelta(), distance: target.getTargetDistance()}))
+	archiver.saveData({
+		position: d
+	})
+	//    targeter.setPosition({
+	//        x: d.longitude,
+	//        y: d.latitude
+	//    })
+	delete d.course
+	comms.send("position", d)
+	comms.send("target", Object.assign({}, {
+		target: targeter.target
+	}, {
+		directionD: targeter.getTargetDirectionDelta(),
+		distance: targeter.getTargetDistance()
+	}))
 })
 
-pilot.enableAutopilot(target)
+pilot.enableAutopilot(targeter)
 
 process.on('SIGINT', () => {
-		camera.kill()
-		sensors.stopAll()
-		if (process.env.DEBUG) process.exit(0)
+	camera.kill()
+	sensors.stopAll()
+	if (process.env.DEBUG) process.exit(0)
 })
