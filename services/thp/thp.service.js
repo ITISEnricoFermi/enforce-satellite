30e147cf7e6f99fd81b722af749957fbd2dab166
n4y0n <e.montada0@gmail.com>
2018-06-18 22:42:36 +0100

    working thp service

--- services/thp/thp.service.js
+++ services/thp/thp.service.js
@@ -1,39 +1,44 @@
-let { MoleculerError } = require("moleculer").Errors;
+let {
+	MoleculerError
+} = require("moleculer").Errors
 
-const Thp = require("./mock/thp");
-const thp = new Thp();
+const Thp = require("./mock/thp")
+const thp = new Thp()
 
 module.exports = {
 	name: "thp",
 	actions: {
 		start(ctx) {
-			if(!thp.running) {
-				this.logger.info("Starting thp");
-				thp.startReading();
+			if (!thp.running) {
+				this.logger.info("Starting thp")
+				thp.startReading()
 				return {
 					state: "started"
-				};
+				}
 			} else {
 				return {
 					state: "already running"
-				};
+				}
 			}
 		},
 		stop(ctx) {
-			if(thp.running) {
-				this.logger.info("Stoping thp");
-				thp.stopReading();
+			if (thp.running) {
+				this.logger.info("Stoping thp")
+				thp.stopReading()
 				return {
 					state: "stopped"
-				};
+				}
 			} else {
 				return {
 					state: "thp not runnig"
-				};
+				}
 			}
 		}
 	},
-
-	events: {
+	created() {
+		this.logger.info("Setting up data event for thp")
+		thp.on("data", data => {
+			this.broker.broadcast("thp.data", data)
+		})
 	}
-};
+}
