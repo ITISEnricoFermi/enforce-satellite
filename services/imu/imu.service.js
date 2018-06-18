1f0a4ad29fb26c3ce9f97dfeb13b16c4afbe7606
n4y0n <e.montada0@gmail.com>
2018-06-18 22:40:57 +0100

    working imu thp services + data events

--- services/imu/imu.service.js
+++ services/imu/imu.service.js
@@ -1,4 +1,4 @@
-let { MoleculerError } = require("moleculer").Errors;
+let { MoleculerError } = require("moleculer").Errors
 
 const Imu = require("./mock/imu")
 const imu = new Imu()
@@ -8,12 +8,25 @@ module.exports = {
 	actions: {
 		start(ctx) {
 			if(!imu.running) {
-				this.logger.info(`Starting Imu`);
+				this.logger.info("Starting Imu")
 				imu.startReading()
 			}
 		},
+		stop(ctx) {
+			if(imu.running) {
+				this.logger.info("Stopping Imu")
+				imu.stopReading()
+			}
+		},
 	},
-
-	events: {
+	created() {
+		this.logger.info("Setting up quaternion event for imu")
+		imu.on("quaternion", data => {
+			this.broker.broadcast("imu.quaternion", data)
+		})
+		this.logger.info("Setting up euler event for imu")
+		imu.on("euler", data => {
+			this.broker.broadcast("imu.euler", data)
+		})
 	}
 }
--- services/thp/thp.js
+++ services/thp/thp.js
@@ -1,47 +1,47 @@
-const BME280 = require("./BME280");
+const BME280 = require("./BME280")
 const {
 	EventEmitter
-} = require("events");
+} = require("events")
 
 
 class THP extends EventEmitter {
 	constructor(delay) {
-		super();
-		this.delay = (delay && !isNaN(delay)) ? delay : 0;
-		this._on = false;
-		this.running = false;
+		super()
+		this.delay = (delay && !isNaN(delay)) ? delay : 0
+		this._on = false
+		this.running = false
 		const options = {
 			i2cBusNo: 0,
 			i2cAddress: BME280.BME280_DEFAULT_I2C_ADDRESS()
-		};
-		this.bme280 = new BME280(options);
+		}
+		this.bme280 = new BME280(options)
 		this.bme280.init((err) => {
-			if (err) return console.error(`BME280 initialization failed: ${err} `);
-			console.log("BME280 initialization succeeded");
-			this._on = true;
-			this.startReading();
-		});
+			if (err) return console.error(`BME280 initialization failed: ${err} `)
+			console.log("BME280 initialization succeeded")
+			this._on = true
+			this.startReading()
+		})
 	}
 
 	readSensorData() {
 		this.to = setTimeout(() => {
 			this.bme280.readSensorData((err, data) => {
-				if (err) this.emit("err", err);
-				else this.emit("data", data);
-				this.readSensorData();
-			});
-		}, this.delay);
+				if (err) this.emit("err", err)
+				else this.emit("data", data)
+				this.readSensorData()
+			})
+		}, this.delay)
 	}
 
 	startReading() {
-		this.running = true;
-		this.readSensorData();
+		this.running = true
+		this.readSensorData()
 	}
 
 	stopReading() {
-		this.running = false;
-		clearTimeout(this.to);
+		this.running = false
+		clearTimeout(this.to)
 	}
 }
 
-module.exports = THP;
+module.exports = THP
