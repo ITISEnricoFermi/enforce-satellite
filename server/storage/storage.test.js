91b87e937f17b807a26b8397a49e4e42f5ae71b3
n4y0n <e.monatada0@gmail.com>
2018-06-27 19:02:04 +0200

    Storage test moved to test dir

--- server/storage/storage.test.js
+++
@@ -1,9 +0,0 @@
-const storage = require("./StorageClass")
-
-const s = new storage("enforce", __dirname+"/hello/")
-
-s.save("humidity", {h: 3242, b: "hellp"}, new Date().getTime())
-s.save("location", {}, new Date().getTime())
-s.save("temperature", {}, new Date().getTime())
-s.save("pressure", {}, new Date().getTime())
-s.save("orientation", {}, new Date().getTime())
