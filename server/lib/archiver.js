fc417d5258898178facabdb171cf7dce0eb48360
n4y0n <e.montada0@gmail.com>
2018-06-29 09:58:21 +0000

    Cooldown added

--- server/lib/archiver.js
+++ server/lib/archiver.js
@@ -18,6 +18,11 @@ class Archiver {
 
 	saveData(data) {
 		if (this.missionId === null) return
+
+		if (!this.timestamps[dataType]) this.timestamps[dataType] = Date.now()
+		else if (Date.now() - this.timestamps[dataType] < this.cooldown) return Promise.resolve()
+		else this.timestamps[dataType] = Date.now()
+
 		return new Promise((resolve, reject) => {
 			data = Object.assign(data, {
 				missionID: this.missionId
--- server/lib/comms.js
+++ server/lib/comms.js
@@ -1,24 +1,24 @@
 const {
-  EventEmitter
+	EventEmitter
 } = require("events")
 
 /**
  * @description Wrapper for XBee module
  */
 class Comms extends EventEmitter {
-  constructor(xbee) {
-    super()
-    if (!xbee) throw new Error(`"xbee is not defined."`)
-    this.xbee = xbee;
-    this.xbee.on("data", d => this.emit("data", d))
-    this.xbee.on("command", c => this.emit("command", c))
-  }
+	constructor(xbee) {
+		super()
+		if (!xbee) throw new Error(`"xbee is not defined."`)
+		this.xbee = xbee;
+		this.xbee.on("data", d => this.emit("data", d))
+		this.xbee.on("command", c => this.emit("command", c))
+	}
 
-  /**
-   * @param {DataType} dataType
-   * @param {DATA} data
-   */
-  send(dataType, data) {
+	/**
+	 * @param {DataType} dataType
+	 * @param {DATA} data
+	 */
+	send(dataType, data) {
 		this.xbee.send(dataType, data)
 	}
 
